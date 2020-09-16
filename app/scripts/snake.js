301c56b59a4d8d5fa9c7946acd74e732d67f88e0
Gabriel García Rodríguez <ggarciaro@minsait.com>
2020-09-16 16:11:27 +0200

    Add snake logic

---
+++ app/scripts/snake.js
@@ -0,0 +1,93 @@
+// Create board settings and direction dictionary
+const boardSize = {
+  x: 31,
+  y: 15,
+};
+
+const directionInfo = {
+  right: {
+    oposite: 'left',
+    vector: [1, 0],
+  },
+  left: {
+    oposite: 'right',
+    vector: [-1, 0],
+  },
+  top: {
+    oposite: 'bottom',
+    vector: [0, 1],
+  },
+  bottom: {
+    oposite: 'top',
+    vector: [0, -1],
+  },
+};
+
+// Create snake and a foodPiece
+const snake = { };
+
+const foodPiece = { };
+
+const moveFood = () => {
+  foodPiece.x = Math.floor(Math.random() * boardSize.x);
+  foodPiece.y = Math.floor(Math.random() * boardSize.y);
+};
+
+const newGame = () => {
+  snake.direction = 'right';
+  snake.body = [
+    { x: 18, y: 8, isFood: false },
+    { x: 17, y: 8, isFood: false },
+    { x: 16, y: 8, isFood: false },
+    { x: 15, y: 8, isFood: false },
+  ];
+  moveFood();
+};
+
+// Snake functionality
+const changeDirection = (newDirection) => {
+  if (newDirection !== directionInfo[snake.direction].oposite) {
+    snake.direction = newDirection;
+  }
+};
+
+const move = () => {
+  const newPosition = {
+    x: snake.body[0].x + directionInfo[snake.direction].vector[0],
+    y: snake.body[0].y + directionInfo[snake.direction].vector[1],
+    isFood: false,
+  };
+
+  if (newPosition.x === boardSize.x) {
+    newPosition.x = 1;
+  }
+
+  if (newPosition.y === boardSize.y) {
+    newPosition.y = 1;
+  }
+
+  if (!snake.body.some((position) => position.x === newPosition.x && position.y === newPosition.y)) {
+    if (newPosition.x === foodPiece.x && newPosition.y === foodPiece.y) {
+      newPosition.isFood = true;
+      moveFood();
+    }
+
+    if (!snake.body[snake.body.length - 1].isFood) {
+      snake.body.splice(-1, 1);
+    }
+
+    snake.body.unshift(newPosition);
+
+    return true;
+  }
+
+  return false;
+};
+
+export {
+  newGame,
+  snake,
+  foodPiece,
+  changeDirection,
+  move,
+};
