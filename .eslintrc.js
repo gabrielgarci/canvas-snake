880303acdf53301ef6f48917519af121b5f44293
Gabriel García Rodríguez <ggarciaro@minsait.com>
2020-09-21 11:05:28 +0200

    Install webpack and restructure the project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,25 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "airbnb-base",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "rules": {
-        "import/extensions": ["error", "always"],
-        "max-len": ["error", { "code": 120 }],
-        "no-unused-expressions": ["error", {"allowTernary": true}],
-        "linebreak-style": ["error", "windows"],
-        "space-in-parens": ["error", "always"],
-        "no-param-reassign": ["error", { "props": false }]
-    }
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": "airbnb-base",
+  "parserOptions": {
+    "ecmaVersion": 12,
+    "sourceType": "module"
+  },
+  "rules": {
+    "import/extensions": ["error", "always"],
+    "max-len": ["error", {
+      "code": 120
+    }],
+    "no-unused-expressions": ["error", {
+      "allowTernary": true
+    }],
+    "linebreak-style": ["error", "windows"],
+    "space-in-parens": ["error", "always"],
+    "no-param-reassign": ["error", {
+      "props": false
+    }]
+  }
 }
